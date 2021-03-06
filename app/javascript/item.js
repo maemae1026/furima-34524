9a5008f94a22749c4ad4a014d0b9447264a0b8bc
maemae1026 <on94.maeda.atsuto@gmail.com>
2021-03-06 15:34:03 +0900

    販売手数料と販売利益の非同期通信

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -1,3 +1,11 @@
-window.addEventListener('load', () => {
-  console.log("OK");
-});
+function check() {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = inputValue/10 - inputValue%10/10
+    const rieki = document.getElementById("profit");
+    rieki.innerHTML = inputValue - (inputValue/10 - inputValue%10/10)
+});}
+
+window.addEventListener("load", check);

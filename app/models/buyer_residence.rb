4ffea8e648e52eaff2007e5d2a45fed9e3ed73ba
maemae1026 <on94.maeda.atsuto@gmail.com>
2021-03-10 15:43:51 +0900

    formオブジェクト作成（記述途中）とbuyerモデル、residenceモデルの作成

---
+++ app/models/buyer_residence.rb
@@ -0,0 +1,16 @@
+class BuyerResidence
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture, :prefecture_id, :city, :house_number, :building_name, :phone_number, :price, :user_id
+
+  # with_options presence: true do
+  #   validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
+  #   validates :user_id
+  #   validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  # end
+  # validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    Buyer = Buyer.create(price: price, user_id: user_id)
+    Residence.create(postal_code: postal_code, prefecture: prefecture, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, residence_id: residence.id)
+  end
+end

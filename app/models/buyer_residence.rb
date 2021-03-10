c92c033e3fada090bd20cf87bac92ddb98e609f5
maemae1026 <on94.maeda.atsuto@gmail.com>
2021-03-10 16:49:45 +0900

    バリデーション作成とbuyersコントローラー作成

--- app/models/buyer_residence.rb
+++ app/models/buyer_residence.rb
@@ -1,13 +1,16 @@
 class BuyerResidence
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :prefecture_id, :city, :house_number, :building_name, :phone_number, :price, :user_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :item_id, :user_id
 
-  # with_options presence: true do
-  #   validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
-  #   validates :user_id
-  #   validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  # end
-  # validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  with_options presence: true do
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :city
+  validates :house_number
+  validates :phone_number, format: {with: /\A[0-9]{,11}\z/}
+  validates :item_id
+  validates :user_id
+  end
 
   def save
     Buyer = Buyer.create(item_id: item.id, user_id: user.id)

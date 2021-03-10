f096ab71de17924274006f47b1f4f65e2fd3bf40
maemae1026 <on94.maeda.atsuto@gmail.com>
2021-03-10 16:29:24 +0900

    カラム追加とアソシエーションの追加。formオブジェクト修正。

--- app/models/buyer_residence.rb
+++ app/models/buyer_residence.rb
@@ -10,7 +10,7 @@ class BuyerResidence
   # validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
-    Buyer = Buyer.create(price: price, user_id: user_id)
-    Residence.create(postal_code: postal_code, prefecture: prefecture, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, residence_id: residence.id)
+    Buyer = Buyer.create(item_id: item.id, user_id: user.id)
+    Residence.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, buyer_id: buyer.id)
   end
 end

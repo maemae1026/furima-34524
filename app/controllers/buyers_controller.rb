097080603e4556b58b912a73d14e8d04e44eb16a
maemae1026 <on94.maeda.atsuto@gmail.com>
2021-03-11 18:43:43 +0900

    フォーム情報保存とエラーメッセージの導入

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,8 +1,22 @@
 class BuyersController < ApplicationController
   def index
     @buyer_residence = BuyerResidence.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @buyer_residence = BuyerResidence.new(buyer_params)
+    if @buyer_residence.valid?
+      @buyer_residence.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def buyer_params
+    params.require(:buyer_residence).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
--- app/models/buyer_residence.rb
+++ app/models/buyer_residence.rb
@@ -13,7 +13,7 @@ class BuyerResidence
   end
 
   def save
-    buyer = Buyer.create(item_id: item.id, user_id: user.id)
+    buyer = Buyer.create(item_id: item_id, user_id: user_id)
     Residence.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, buyer_id: buyer.id)
   end
 end

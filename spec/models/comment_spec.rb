3c7d96184d9e29209a58469e469d899186d48c6b
maemae1026 <on94.maeda.atsuto@gmail.com>
2021-03-18 10:11:36 +0900

    コメントテストコード（エラー表示文修正前）

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '商品購入' do
+    before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  context 'コメントできるとき' do
+    it 'user_idとitem_idとテキストに文字があれば保存できること' do
+      expect(@comment).to be_valid
+    end
+  end
+  context 'コメントできないとき' do
+    it 'コメントの保存はテキストが空では保存できないこと' do
+      @comment.text = ''
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include 'Comment is invalid'
+    end
+    it 'userが紐付いていないと保存できないこと' do
+      @comment.user = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include('User must exist')
+end
+it 'itemが紐付いていないと保存できないこと' do
+  @comment.item = nil
+  @comment.valid?
+  expect(@comment.errors.full_messages).to include('Item translation missing: ja.activerecord.errors.models.comment.attributes.item.required')
+end
+end
+end
 end
